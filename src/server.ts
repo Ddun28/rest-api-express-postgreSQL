77a19479786ed8ebdaa67c6d7251c0ce0d385089
¨Daniel <danieldum28@gmail.com>
2024-08-28 22:11:13 -0400

    fix

--- src/server.ts
+++ src/server.ts
@@ -26,13 +26,10 @@ const server = express()
 
 // Permitir conexiones
 const corsOptions : CorsOptions = {
-  origin: function(origin, callback){
-    if(origin === process.env.FRONTEND_URL ){
-       callback(null, true)
-    }else {
-      callback(new Error('Error de CORS'))
-    }     
-  }
+  origin: function(origin, callback) {
+    const whitelist = [process.env.FRONTEND_URL]
+    callback(null, true)
+}
 }
 
 server.use(cors(corsOptions))
